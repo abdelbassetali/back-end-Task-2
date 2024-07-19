5e970360da639364622b867819d55bd60c44daec
abdelbassetali <146743316+abdelbassetali@users.noreply.github.com>
2024-07-19 11:26:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const request = require("request")
+
+
+const geocode = require("./geaodata")
+const forecast = require('./forcast')
+const country = process.argv[2]
+geocode(country, (error, data) => {
+   console.log("Error:", error)
+   console.log("data:", data)
+
+   forecast(data.longitude, data.latitude, (error, data) => {
+      console.log(error)
+      console.log(data)
+   })
+})
